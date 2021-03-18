d761d5df99ffd882b7cb34a004b557586dbf6798
nahsooyeon <nahsooyeon@gmail.com>
2021-03-18 23:02:40 +0900

    ⚒  수정 : 프리티어 적용 및 테스팅을 위한 컴포넌트 주석 처리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Nav from "./Components/Nav";
 import "./App.css";
+import Login from "./Components/Login/Login";
 import CourseList from "./Components/Main/CourseList";
 
 function App() {
   return (
     <>
-      <Nav />
-      <CourseList />
+      {/* <Nav />
+      /*<CourseList /> */}
+      <Login />
     </>
   );
 }
