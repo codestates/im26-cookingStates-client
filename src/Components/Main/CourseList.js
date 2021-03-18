d1534c50c80ec9b7100b0938b53ed5d5c155b390
nahsooyeon <nahsooyeon@gmail.com>
2021-03-18 16:51:30 +0900

    ♻️  리팩토링 : CourseList.js 주석 삭제

--- src/Components/Main/CourseList.js
+++ src/Components/Main/CourseList.js
@@ -11,17 +11,6 @@ function CourseList() {
       {courseListData.map((course) => (
         <CourseListItem course={course} key={course.id} />
       ))}
-
-      {/*  <div className="course-list-item">
-        <div className="course-img">
-          <img src={koreanImg} alt="한식 코스" />
-        </div>
-        <div>
-          <div className="course-title">한식</div>
-          <div className="course-desc">매일 먹는 집밥 만들기</div>
-          <button className="btn-learn">지금 학습하기</button>
-        </div>
-      </div> */}
     </div>
   );
 }
