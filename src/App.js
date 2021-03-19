78ae5220d7f64bbe2f768a374ee646da5d9991de
oh930428 <oh930428@gmail.com>
2021-03-19 16:05:43 +0900

    🔨 수정 : App.js 소스코드수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-
 // import Nav from './Components/Nav';
 import './App.css';
 // import CourseList from './Components/Main/CourseList';
-import Unregister from './pages/unregister';
-
+// import Unregister from './pages/unregister';
+import Unregister_yes from './pages/unregister_yes';
 
 function App() {
   return (
     <>
-      <Unregister />
+      <Unregister_yes />
     </>
   );
 }
