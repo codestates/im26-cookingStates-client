1018b25e74ebfa10b7ea4c690467d692109ffe4a
Yeonju Lee <jessy2yon@gmail.com>
2021-03-19 23:12:17 +0900

    🔨 수정 : Course 컴포넌트 추가, api 파일 추가

---
+++ src/Components/Course/MenuList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import MenuCard from './MenuCard';
+import '../../pages/CSS/content-coursetitle.css';
+import axios from 'axios';
+import API from '../../api';
+
+function MenuList() {
+  const [Recipes, setRecipes] = useState([]);
+  const [CourseId, setCourseId] = useState(1); // ! state로 하지 않고 props로 받아와야 함
+
+  useEffect(() => {
+    getRecipes(CourseId);
+    //console.log(Recipes);
+  }, []);
+
+  const getRecipes = (courseId) => {
+    axios.get(API.COURSE_INFO + '/' + courseId).then((res) => {
+      //console.log(res.data);
+      setRecipes(res.data);
+    });
+  };
+
+  return (
+    <div className="menu-list">
+      {Recipes.map((recipe) => (
+        <MenuCard recipe={recipe} key={recipe.id} />
+      ))}
+    </div>
+  );
+}
+
+export default MenuList;
