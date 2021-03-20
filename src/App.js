3840572b68f3f022205d6dfbe06d28d2b02bfd96
oh930428 <oh930428@gmail.com>
2021-03-20 21:29:44 +0900

    🔨 추가 : App.js, Nav.js 소스코드 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-
-// import Nav from './Components/Nav';
 import './App.css';
+// import Nav from './Components/Nav';
 // import CourseList from './Components/Main/CourseList';
-import Unregister from './pages/unregister';
-
+// import Unregister from './pages/unregister';
+import MyInfo from './pages/myinfo';
 
 function App() {
   return (
     <>
-      <Unregister />
+      <MyInfo />
     </>
   );
 }
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "../Images/logo-1.png";
-import "../pages/CSS/Nav.css";
+import React from 'react';
+import logo from '../../Images/logo-1.png';
+import '../../pages/CSS/nav.css';
 
 function Nav(props) {
   return (
@@ -11,7 +11,7 @@ function Nav(props) {
 
       <div className="nav-btn-group">
         <button className="btn-login">Log in</button>
-        <button className="btn-signup">Sign up</button>
+        <button className="btn-signup">Sign Up</button>
       </div>
     </div>
   );
