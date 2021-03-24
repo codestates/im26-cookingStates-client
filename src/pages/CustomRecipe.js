1fc95a2ef67e38cc577f81e20617ecb2d5af7113
osunguk(mac) <osunguk@gmail.com>
2021-03-24 22:25:43 +0900

    ✨ 추가기능 : 커스텀 레시피 등록 페이지

--- src/pages/CustomRecipe.js
+++ src/pages/CustomRecipe.js
@@ -1,25 +1,41 @@
 import { withRouter } from "react-router-dom";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import "./CSS/CustomRecipe.css";
+import axios from "axios";
+import API from "../api";
 
 function CustomRecipe() {
-  const [Title, setTitle] = useState("");
-  const [Difficulty, setDifficulty] = useState("");
-  const [Manual, setManual] = useState([]);
-  const [FoodImage, setFoodImage] = useState("");
-  const [Type, setType] = useState("");
+  const userInfo = useSelector((state) => state.userReducer.userInfo);
+
+  const [inputs, setInputs] = useState({
+    title: "",
+    file: "",
+    difficulty: "",
+    type: "",
+    manual: "",
+  });
+  console.log(userInfo.data.email);
+
+  const onChangeHandler = (e) => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value 로 설정
+    });
+  };
 
   return (
     <div className="custom-recipe-page">
       <h1>커스텀 레시피 등록 페이지</h1>
-      <form>
+      <div className="input-box">
         <label>
           요리 제목 :
           <input
             type="text"
             name="title"
-            onChange={(e) => setTitle(e.target.value)}
-            value={Title}
+            onChange={onChangeHandler}
+            value={inputs.title}
           ></input>
         </label>
 
@@ -31,11 +47,16 @@ function CustomRecipe() {
             name="image"
             onChange={(e) => {
               const file = e.target.files[0];
+              setInputs({
+                ...inputs,
+                file: file,
+              });
               const reader = new FileReader();
               reader.readAsDataURL(file);
 
               reader.onload = function () {
                 let photoFrame = document.createElement("div");
+
                 photoFrame.style = `background : url(${reader.result}); background-size : cover`;
                 photoFrame.className = "photoFrame";
                 document.getElementById("pictures").appendChild(photoFrame);
@@ -57,8 +78,8 @@ function CustomRecipe() {
             max="5"
             type="number"
             name="difficulty"
-            onChange={(e) => setDifficulty(e.target.value)}
-            value={Difficulty}
+            onChange={onChangeHandler}
+            value={inputs.difficulty}
           ></input>
         </label>
 
@@ -67,17 +88,64 @@ function CustomRecipe() {
           <input
             type="text"
             name="type"
-            onChange={(e) => setType(e.target.value)}
-            value={Type}
+            onChange={onChangeHandler}
+            value={inputs.type}
           ></input>
         </label>
 
         <label>
-          요리 방법 :<input type="text" name="type"></input>
+          요리 방법 :
+          <textarea
+            cols="30"
+            rows="4"
+            name="manual"
+            value={inputs.manual}
+            onChange={onChangeHandler}
+          ></textarea>
         </label>
-        <button>요리방법 추가하기</button>
-        <button type="submit">등록하기</button>
-      </form>
+        <button
+          onClick={() => {
+            if (inputs.manual && inputs.title && inputs.type) {
+              const formData = new FormData();
+              formData.append("file", inputs.file);
+              let temp = Object.assign(
+                {},
+                {
+                  ...inputs,
+                  author: userInfo.data.userName,
+                  email: userInfo.data.email,
+                }
+              );
+              delete temp.file;
+              console.log(temp);
+              formData.append("data", JSON.stringify(temp));
+              axios
+                .post(API.CUSTOM_RECIPE_UPLOAD, formData, {
+                  headers: {
+                    "Content-Type": "multipart/form-data",
+                  },
+                })
+                .then((res) => {
+                  setInputs({
+                    title: "",
+                    file: "",
+                    difficulty: "",
+                    type: "",
+                    manual: "",
+                  });
+                })
+                .catch((e) => {
+                  console.log("error!", e);
+                });
+            } else {
+              alert("데이터를 모두 입력해주세요");
+            }
+          }}
+        >
+          등록하기
+        </button>
+        <form></form>
+      </div>
     </div>
   );
 }
